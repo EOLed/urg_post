9433c25cb1d558472519ab5cb30d9e3b8be4959a
Amos Chan <amos.chan+github@chapps.org>
2011-01-05 18:26:58 -0500

    Copies contents from SVN.

---
+++ controllers/posts_controller.php
@@ -0,0 +1,70 @@
+<?php
+class PostsController extends UrgPostAppController {
+
+	var $name = 'Posts';
+
+	function index() {
+		$this->Post->recursive = 0;
+		$this->set('posts', $this->paginate());
+	}
+
+	function view($id = null) {
+		if (!$id) {
+			$this->Session->setFlash(__('Invalid post', true));
+			$this->redirect(array('action' => 'index'));
+		}
+		$this->set('post', $this->Post->read(null, $id));
+	}
+
+	function add() {
+		if (!empty($this->data)) {
+			$this->Post->create();
+            $post_creator = $this->Auth->user();
+            $this->data["User"] = $post_creator["User"];
+            $this->log("new post created by: " . Debugger::exportVar($post_creator["User"]), LOG_DEBUG);
+			if ($this->Post->saveAll($this->data)) {
+				$this->Session->setFlash(__('The post has been saved', true));
+				$this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The post could not be saved. Please, try again.', true));
+			}
+		}
+		$groups = $this->Post->Group->find('list');
+		$this->set(compact('groups'));
+	}
+
+	function edit($id = null) {
+		if (!$id && empty($this->data)) {
+			$this->Session->setFlash(__('Invalid post', true));
+			$this->redirect(array('action' => 'index'));
+		}
+		if (!empty($this->data)) {
+			if ($this->Post->save($this->data)) {
+				$this->Session->setFlash(__('The post has been saved', true));
+				$this->redirect(array('action' => 'index'));
+			} else {
+				$this->Session->setFlash(__('The post could not be saved. Please, try again.', true));
+			}
+		}
+		if (empty($this->data)) {
+			$this->data = $this->Post->read(null, $id);
+		}
+		$groups = $this->Post->Group->find('list');
+		$users = $this->Post->User->find('list');
+		$this->set(compact('groups', 'users'));
+	}
+
+	function delete($id = null) {
+		if (!$id) {
+			$this->Session->setFlash(__('Invalid id for post', true));
+			$this->redirect(array('action'=>'index'));
+		}
+		if ($this->Post->delete($id)) {
+			$this->Session->setFlash(__('Post deleted', true));
+			$this->redirect(array('action'=>'index'));
+		}
+		$this->Session->setFlash(__('Post was not deleted', true));
+		$this->redirect(array('action' => 'index'));
+	}
+}
+?>
---
+++ views/posts/add.ctp
@@ -0,0 +1,23 @@
+<div class="posts form">
+<?php echo $this->Form->create('Post');?>
+	<fieldset>
+ 		<legend><?php __('Add Post'); ?></legend>
+	<?php
+		echo $this->Form->input('group_id');
+		echo $this->Form->input('title');
+		echo $this->Form->input('content');
+	?>
+	</fieldset>
+<?php echo $this->Form->end(__('Submit', true));?>
+</div>
+<div class="actions">
+	<h3><?php __('Actions'); ?></h3>
+	<ul>
+
+		<li><?php echo $this->Html->link(__('List Posts', true), array('action' => 'index'));?></li>
+		<li><?php echo $this->Html->link(__('List Groups', true), array('controller' => 'groups', 'action' => 'index')); ?> </li>
+		<li><?php echo $this->Html->link(__('New Group', true), array('controller' => 'groups', 'action' => 'add')); ?> </li>
+		<li><?php echo $this->Html->link(__('List Users', true), array('controller' => 'users', 'action' => 'index')); ?> </li>
+		<li><?php echo $this->Html->link(__('New User', true), array('controller' => 'users', 'action' => 'add')); ?> </li>
+	</ul>
+</div>
