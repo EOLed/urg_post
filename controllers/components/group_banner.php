56f241ed243d725cc8eb5b1d2185e3cdf18b8759
Amos Chan <amos.chan@chapps.org>
2011-07-26 01:21:19 -0400

    Parent Group banner fallback

--- controllers/components/group_banner.php
+++ controllers/components/group_banner.php
@@ -8,20 +8,38 @@ App::import("Lib", "Urg.AbstractWidgetComponent");
 class GroupBannerComponent extends AbstractWidgetComponent {
     function build_widget() {
         $this->bindModels();
-        $config = $this->controller->Group->find("first", 
-                array("conditions" => 
-                        array("ParentGroup.id" => $this->widget_settings["group_id"],
-                              "I18n__name.content" => __("Config", true)
-                        )
-                )
-        );
+        $group_id = $this->widget_settings["group_id"];
+        $banners = $this->get_banners($group_id);
 
-        $banners = $this->controller->Post->find("first", array("conditions" => array("Post.group_id" => $config["Group"]["id"],
-                                                                          "I18n__title.content" => __("Banners", true))));
+        while ($banners === false) {
+            $parent = $this->controller->Group->getparentnode($group_id);
+            CakeLog::write("debug", "parent of group ($group_id): " . Debugger::exportVar($parent, 3));
+
+            if ($parent !== false) {
+                $group_id = $parent["Group"]["id"];
+                $banners = $this->get_banners($group_id);
+            } else {
+                break;
+            }
+        }
 
         $this->set_banners($banners);
     }
 
+    function get_banners($group_id) {
+        $banners = false;
+        $config = $this->controller->Group->find("first", 
+                array("conditions" => array("ParentGroup.id" => $group_id, 
+                                            "I18n__name.content" => __("Config", true))));
+
+        if ($config !== false) {
+            $banners = $this->controller->Post->find("first", 
+                    array("conditions" => array("Post.group_id" => $config["Group"]["id"],
+                                                "I18n__title.content" => __("Banners", true))));
+        }
+
+        return $banners;
+    }
     function bindModels() {
         $this->controller->loadModel("Attachment");
         $this->controller->loadModel("UrgPost.Post");
