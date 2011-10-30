b48bac891df228a1271382a63a5b71ae3af2728f
Amos Chan <amos.chan@chapps.org>
2011-10-30 17:04:40 -0400

    "Read More" for recent activity posts

--- views/helpers/recent_activity.php
+++ views/helpers/recent_activity.php
@@ -7,6 +7,7 @@ class RecentActivityHelper extends AppHelper {
     function build($options = array()) {
         $this->options = $options;
         $this->Html->css("/urg_post/css/urg_post.css", null, array("inline"=>false));
+        $this->Html->script("/urg_post/js/jquery.expander.min", array("inline" => false));
         $title = $this->Html->tag("h2", __($options["recent_activity_title"], true));
         return $this->Html->div("recent-activity", $title . $this->post_feed($options["recent_activity"]));
     }
@@ -37,9 +38,24 @@ class RecentActivityHelper extends AppHelper {
                                             $feed_item["Post"]["id"],
                                             $feed_item["Post"]["slug"]), 
                                       array("class"=>"post-title")));
-            $feed .= $banner . $this->Html->div("activity-feed-post post", $title . $this->Markdown->html($feed_item["Post"]["content"]) . $time);
+            $post_content = $this->Html->div("activity-feed-post-content", 
+                                             $this->Markdown->html($feed_item["Post"]["content"]),
+                                             array("id" => "activity-feed-post-content-" . $feed_item["Post"]["id"]));
+            $feed .= $banner . $this->Html->div("activity-feed-post post", $title . $post_content . $this->js($feed_item["Post"]["id"]) . $time);
         }
 
         return $this->Html->div("", $feed, array("id" => "activity-feed"));
+    }
+
+    function js($post_id) {
+        return $this->Html->scriptBlock("
+            $('#activity-feed-post-content-$post_id').expander({
+                slicePoint: 250,
+                preserveWords: true,
+                widow: 50,
+                userCollapseText: '',
+                expandText: '" . __("See More", true) . "',
+                expandPrefix: '<br/>...<br/>'
+            });", array("inline" => true));
     }
 }
